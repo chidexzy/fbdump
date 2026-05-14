18e53e7ea8c071493bd8300047845905c640e8de
Promise Okwudire <66396748+chidexzy@users.noreply.github.com>
2026-05-14 15:56:42 +0200

    Add files via upload

---
+++ Nokia.py
@@ -0,0 +1,44 @@
+import os, sys, platform, time
+from time import sleep
+try:
+    import requests
+except:
+    os.system('pip install requests')
+def brand():
+	os.system('clear')
+	print("""
+██████╗ ██████╗ ███████╗██╗  ██╗
+██╔══██╗██╔══██╗██╔════╝╚██╗██╔╝
+██║  ██║██████╔╝█████╗   ╚███╔╝ 
+██║  ██║██╔══██╗██╔══╝   ██╔██╗ 
+██████╔╝██║  ██║███████╗██╔╝ ██╗
+╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+                                """)
+	time.sleep(1)
+	os.system('clear')
+	print("""
+██╗███████╗
+██║██╔════╝
+██║███████╗
+██║╚════██║
+██║███████║
+╚═╝╚══════╝
+           """)
+	time.sleep(1)
+	os.system('clear')
+	print("""
+██╗  ██╗██╗███╗   ██╗ ██████╗ 
+██║ ██╔╝██║████╗  ██║██╔════╝ 
+█████╔╝ ██║██╔██╗ ██║██║  ███╗
+██╔═██╗ ██║██║╚██╗██║██║   ██║
+██║  ██╗██║██║ ╚████║╚██████╔╝
+╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ 
+                              """)
+	time.sleep(1)
+	os.system('clear')
+brand()
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import Nokia64
+elif bit == '32bit':
+    import Nokia64
